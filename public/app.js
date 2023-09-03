75a8d5c4e4172dea3feff067afec621be54503dc
Iamneo <techadmin@iamneo.ai>
2023-09-03 15:43:57 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
 function myfunc() {
 
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
-    
+    b1 = document.getElementById("b1").Value
+    b2 = document.getElementById("b2").Value;
+    b3 = document.getElementById("b3").Value;
+    b4 = document.getElementById("b4").Value;
+    b5 = document.getElementById("b5").Value;
+    b6 = document.getElementById("b6").Value;
+    b7 = document.getElementById("b7").Value;
+    b8 = document.getElementById("b8").Value;
+    b9 = document.getElementById("b9").Value;
+
+    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    b1 = document.getElementById("b1").Value
+    b2 = document.getElementById("b2").Value;
+    b3 = document.getElementById("b3").Value;
+    b4 = document.getElementById("b4").Value;
+    b5 = document.getElementById("b5").Value;
+    b6 = document.getElementById("b6").Value;
+    b7 = document.getElementById("b7").Value;
+    b8 = document.getElementById("b8").Value;
+    b9 = document.getElementById("b9").Value;
 }
