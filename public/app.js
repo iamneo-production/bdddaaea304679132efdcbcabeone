6eca91001cdee9fb11cfe2d6492a076b6633f2fe
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:18:57 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -147,19 +147,22 @@ function myfunc() {
            b4btn.style.color = "red";
            b7btn.style.color = "red";
        }
-       else if((b1 == 'x') || (b1 == 'X') && (b4 == 'x') || (b4 == 'X') && (b7 == 'x') || (b7 == 'X')){
+       else if((b7 == 'x') || (b3 == 'X') && (b6 == 'x') || (b6 == 'X') && (b9 == 'x') || (b9 == 'X')){
         document.getElementById('print')
            .innerHTML = "player X won";
        
+           b1btn.disabled = true;
            b2btn.disabled = true;
-           b3btn.disabled = true;
+           b4btn.disabled = true;
            b5btn.disabled = true;
-           b6btn.disabled = true;
+           b7btn.disabled = true;
            b8btn.disabled = true;
-           b9btn.disabled = true;
            
-           b1btn.style.color = "red";
-           b4btn.style.color = "red";
-           b7btn.style.color = "red";
+           b3btn.style.color = "red";
+           b6btn.style.color = "red";
+           b9btn.style.color = "red";
        }
+       
+       
+
 }
