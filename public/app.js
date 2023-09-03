f9c2de34da926394a0e361298e4cc576944917cf
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:08:57 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -53,5 +53,51 @@ function myfunc() {
         b7btn.style.color = "red";
     }
 
-    
+    else if((b7 == 'x') || (b7 == 'X') && (b8 == 'x') || (b8 == 'X') && (b9 == 'x') || (b9 == 'X')){
+        document.getElementById('print')
+           .innerHTML = "player X won";
+       
+           b1btn.disabled = true;
+           b2btn.disabled = true;
+           b3btn.disabled = true;
+           b4btn.disabled = true;
+           b5btn.disabled = true;
+           b6tn.disabled = true;
+           
+           b7btn.style.color = "red";
+           b8btn.style.color = "red";
+           b9btn.style.color = "red";
+       }
+
+       else if((b1 == 'x') || (b1 == 'X') && (b5 == 'x') || (b5 == 'X') && (b9 == 'x') || (b9 == 'X')){
+        document.getElementById('print')
+           .innerHTML = "player X won";
+       
+           b1btn.disabled = true;
+           b2btn.disabled = true;
+           b4btn.disabled = true;
+           b6btn.disabled = true;
+           b8btn.disabled = true;
+           b9tn.disabled = true;
+           
+           b3btn.style.color = "red";
+           b5btn.style.color = "red";
+           b7btn.style.color = "red";
+       }
+
+       else if((b2 == 'x') || (b2 == 'X') && (b5 == 'x') || (b5 == 'X') && (b8 == 'x') || (b8 == 'X')){
+        document.getElementById('print')
+           .innerHTML = "player X won";
+       
+           b1btn.disabled = true;
+           b2btn.disabled = true;
+           b4btn.disabled = true;
+           b6btn.disabled = true;
+           b7btn.disabled = true;
+           b9btn.disabled = true;
+           
+           b2btn.style.color = "red";
+           b5btn.style.color = "red";
+           b8btn.style.color = "red";
+       }
 }
