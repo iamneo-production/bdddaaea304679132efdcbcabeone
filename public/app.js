0dba64b28fc2b6cdce9964a4a2e490170a6b33b1
Iamneo <techadmin@iamneo.ai>
2023-09-03 15:48:57 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -11,14 +11,16 @@ function myfunc() {
     b8 = document.getElementById("b8").Value;
     b9 = document.getElementById("b9").Value;
 
-    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
-    b1 = document.getElementById("b1").Value
-    b2 = document.getElementById("b2").Value;
-    b3 = document.getElementById("b3").Value;
-    b4 = document.getElementById("b4").Value;
-    b5 = document.getElementById("b5").Value;
-    b6 = document.getElementById("b6").Value;
-    b7 = document.getElementById("b7").Value;
-    b8 = document.getElementById("b8").Value;
-    b9 = document.getElementById("b9").Value;
+    var b1btn, b2btn, b3btn, b4btn, b5btn, b6btn, b7btn, b8btn, b9btn;
+    b1btn = document.getElementById("b1").Value
+    b2btn = document.getElementById("b2").Value;
+    b3btn = document.getElementById("b3").Value;
+    b4btn = document.getElementById("b4").Value;
+    b5btn = document.getElementById("b5").Value;
+    b6btn = document.getElementById("b6").Value;
+    b7btn = document.getElementById("b7").Value;
+    b8btn = document.getElementById("b8").Value;
+    b9btn = document.getElementById("b9").Value;
+    
+    # ||
 }
